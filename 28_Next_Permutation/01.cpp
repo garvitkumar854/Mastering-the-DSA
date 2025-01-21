83d961dbc9dbce37155881295eddbc2f68eaf0cb
GarvitKumar854 <garvitkajot854@gmail.com>
2025-01-21 15:55:22 +0530

    Updated Next Permutaion

--- 28_Next_Permutation/01.cpp
+++ 28_Next_Permutation/01.cpp
@@ -23,21 +23,21 @@ Taking Example: [1,2,3,5,4]     ->  [1,2,4,3,5]
                 [1,2,5,4,3]     ->  [1,3,2,4,5]
 Notice the Element {Backward Travels}:
 4<5 but {5>3} in 1st E.g(Qs),     4<5,5<6, but {6>3} (first element that we are encountering)
-similarly 3<4,4<5, but {2>5} called pivotot in Permutation      
-Characterstic of pivotot: A[i] < A[i+1] {if Backward Travels}
-Step 1: Find the pivotot
-Step 2: Find the righmost element > pivotot element
-Step 3: Swap pivotot with rightmost element.
-Step 4: Reverse elements from (pivotot+1) to n-1.
+similarly 3<4,4<5, but {2>5} called pivot in Permutation      
+Characterstic of pivot: A[i] < A[i+1] {if Backward Travels}
+Step 1: Find the pivot
+Step 2: Find the righmost element > pivot element
+Step 3: Swap pivot with rightmost element.
+Step 4: Reverse elements from (pivot+1) to n-1.
 
-If in a Situation that pivotot not exits so we will arrange array in ascending order {Question Says}
+If in a Situation that pivot not exits so we will arrange array in ascending order {Question Says}
 {5,4,3,2,1} (Next Permutation Never exits) --> {1,2,3,4,5}
 */
 
 vector<int> nextPermutation(vector<int> &arr){          // {TC = O(n), SC = O(1)}
     int n = arr.size();
 
-    // Code to find the pivotot {TC = O(n)}
+    // Code to find the pivot {TC = O(n)}
     int pivot = -1;
     for(int i=n-2; i>=0; i--){
         if(arr[i] < arr[i+1]){
@@ -51,15 +51,15 @@ vector<int> nextPermutation(vector<int> &arr){          // {TC = O(n), SC = O(1)
         return arr;
     }
 
-    // Find Rightmost Element which is > pivotot.
+    // Find Rightmost Element which is > pivot.
     for(int i = n-1; i>pivot; i--){
         if(arr[i] > arr[pivot]){
-            swap(arr[i], arr[pivot]);     // swap the element with pivotot
+            swap(arr[i], arr[pivot]);     // swap the element with pivot
             break;
         }
     }
 
-    // Reverse the Part after pivotot to n-1.
+    // Reverse the Part after pivot to n-1.
     // reverse(arr.begin() + pivot+1, arr.end())        --> Alternative
     int i = pivot + 1, j = n-1;
     while(i <= j){
